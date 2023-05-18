5e5fb958567f9646cad208e176ff8a320609b1b4
junio10 <junio.js977@gmail.com>
2023-05-18 18:43:42 -0300

    feito ate o exercicio 10. Onde já foi abordado conceito de like, order by, in, not in, or, and

--- consultasIniciantes.sql
+++ consultasIniciantes.sql
@@ -21,3 +21,32 @@ select E.FirstName, E.LastName, E.HireDate from Employees E where E.Title = 'Sal
 
 --5 Show all the orders placed by a specific employee.
 select O.OrderID, O.OrderDate from Orders O where O.EmployeeID = 5 
+
+--6 In the Suppliers table, show the SupplierID,
+--ContactName, and ContactTitle for those Suppliers
+--whose ContactTitle is not Marketing Manager
+select S.SupplierID, S.ContactName, S.ContactTitle from Suppliers S WHERE S.ContactTitle NOT IN ( 'Marketing Manager')
+
+
+--7 In the products table, we’d like to see the ProductID
+--and ProductName for those products where the
+--ProductName includes the string “queso”.
+select * from Products P WHERE P.ProductName LIKE 'Queso%' --%indica qualquer coisa adiante,
+                                                           -- EX: PODERIA SER TODOS OS PRODUTOS QUE TERMINA COM O
+														   -- select * from Products P WHERE P.ProductName LIKE '%o'
+														   
+--8 Looking at the Orders table, there’s a field called
+--ShipCountry. Write a query that shows the OrderID,
+--CustomerID, and ShipCountry for the orders where
+--the ShipCountry is either France or Belgium
+select O.OrderID,O.CustomerID, O.ShipCountry from Orders O 
+WHERE O.ShipCountry IN ('Belgium','France')
+
+--9 Orders shipping to any country in Latin America
+select * from Orders O WHERE ShipCountry IN ('Brazil', 'Venezuela', 'Argentina', 'Mexico')--em vez de usar vários OR, É mais simples utilizar in
+                                                                                          --Mesmo resultado ex: 
+																						  --select * from Orders O 
+																						  --WHERE ShipCountry ='Brazil' or ShipCountry = 'Venezuela' 
+																						  --or  ShipCountry = 'Argentina'or ShipCountry = 'Mexico'
+--10 Employees, in order of age
+SELECT E.FirstName, E.LastName, E.Title, E.BirthDate FROM Employees E Order by E.BirthDate ASC
