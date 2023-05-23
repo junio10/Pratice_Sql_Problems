6017082c65bd18e6008d1171099cceeb65e908ba
junio10 <junio.js977@gmail.com>
2023-05-23 15:51:18 -0300

    feature: feito ate a questao 25

--- consultasIntermediarias.sql
+++ consultasIntermediarias.sql
@@ -19,6 +19,26 @@ ORDER BY P.ProductID ASC
 
 --23 Products that need reordering,
 --continued
-SELECT P.ProductID ,P.ProductName, P.UnitsInStock, P.UnitsOnOrder, P.ReorderLevel FROM Products P
+SELECT P.ProductID ,P.ProductName, P.UnitsInStock, P.UnitsOnOrder, P.ReorderLevel, P.Discontinued FROM Products P
 WHERE P.UnitsInStock + P.UnitsOnOrder <= P.ReorderLevel AND P.Discontinued = 0
 ORDER BY P.ProductID ASC
+
+--24 Customer list by region
+SELECT C.CustomerID, C.CompanyName, C.Region
+FROM Customers C
+Order By
+      Case
+		when C.Region is null then 1
+		else 0
+      End
+	,Region
+	,CustomerID
+
+--25 High freight charges
+SELECT O.ShipCountry, AVG(O.Freight) AS AVGFREIGHT FROM Orders O
+GROUP BY O.ShipCountry
+ORDER BY AVG(O.Freight) DESC
+OFFSET 0 ROWS FETCH FIRST 3 ROWS ONLY
+
+
+
